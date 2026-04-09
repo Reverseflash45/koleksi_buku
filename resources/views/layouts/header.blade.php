41ffcb677e50ee7e60b4d97fc4ed0d53fe13743b
Reverseflash45 <rffernanditoo@gmail.com>
2026-04-09 11:51:20 +0700

    update terbaru

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,15 @@
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Purple Admin</title>
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
-  </head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Purple Admin</title>
+
+  <link rel="stylesheet" href="{{ asset('assets/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/assets/vendors/ti-icons/css/themify-icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/assets/vendors/css/vendor.bundle.base.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/assets/vendors/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
+
+  <link rel="stylesheet" href="{{ asset('assets/assets/css/style.css') }}">
+
+  <link rel="shortcut icon" href="{{ asset('assets/assets/images/favicon.png') }}" />
+</head>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,65 +1,76 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-          <ul class="nav">
-            <li class="nav-item nav-profile">
-              <a href="#" class="nav-link">
+    <ul class="nav">
+        <li class="nav-item nav-profile">
+            <a href="#" class="nav-link">
                 <div class="nav-profile-image">
-                  <img src="assets/images/faces/face1.jpg" alt="profile" />
-                  <span class="login-status online"></span>
-                  </div>
+                    {{-- PERBAIKAN PATH FOTO PROFIL SIDEBAR --}}
+                    <img src="{{ asset('assets/assets/images/faces/face1.jpg') }}" alt="profile" />
+                    <span class="login-status online"></span>
+                </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">Rafi Fernandito</span>
-                  <span class="text-secondary text-small">Project Manager</span>
+                    <span class="font-weight-bold mb-2">
+                        @auth {{ Auth::user()->name }} @else Guest @endauth
+                    </span>
+                    <span class="text-secondary text-small">Project Manager</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-              </a>
-            <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/') }}">
-        <span class="menu-title">Dashboard</span>
-    </a>
-</li>
+            </a>
+        </li> {{-- TUTUP TAG LI PROFILE --}}
 
-<li class="nav-item {{ request()->is('kategori*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/kategori') }}">
-        <span class="menu-title">Kategori</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/') }}">
+                <span class="menu-title">Dashboard</span>
+                <i class="mdi mdi-home menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item {{ request()->is('buku*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/buku') }}">
-        <span class="menu-title">Buku</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('kategori*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/kategori') }}">
+                <span class="menu-title">Kategori</span>
+                <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item {{ request()->is('pdf*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/pdf') }}">
-        <span class="menu-title">pdf</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('buku*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/buku') }}">
+                <span class="menu-title">Buku</span>
+                <i class="mdi mdi-book-open-variant menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item {{ request()->is('barang*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/barang') }}">
-        <span class="menu-title">Cetak Data</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('pdf*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/pdf') }}">
+                <span class="menu-title">pdf</span>
+                <i class="mdi mdi-file-pdf menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item {{ request()->is('kota*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ url('/kota') }}">
-        <span class="menu-title">Kota</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('barang*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/barang') }}">
+                <span class="menu-title">Cetak Data</span>
+                <i class="mdi mdi-printer menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item">
-    <a class="nav-link" href="{{ url('/wilayah') }}">
-        <span class="menu-title">Wilayah</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('kota*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/kota') }}">
+                <span class="menu-title">Kota</span>
+                <i class="mdi mdi-map-marker menu-icon"></i>
+            </a>
+        </li>
 
-<li class="nav-item">
-    <a class="nav-link" href="{{ url('/pos') }}">
-        <span class="menu-title">POS</span>
-    </a>
-</li>
+        <li class="nav-item {{ request()->is('wilayah*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/wilayah') }}">
+                <span class="menu-title">Wilayah</span>
+                <i class="mdi mdi-map menu-icon"></i>
+            </a>
+        </li>
 
-          </ul>
-        </nav>
-
+        <li class="nav-item {{ request()->is('pos*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('/pos') }}">
+                <span class="menu-title">POS</span>
+                <i class="mdi mdi-cart menu-icon"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
